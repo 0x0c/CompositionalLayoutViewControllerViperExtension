b46e397444fa8ccaa6e664803d1299a01bbd3915
Akira <akira.matsuda@me.com>
2022-03-13 23:24:26 +0900

    Add StoreBuilder

--- Sources/CollectionViewInteractorInput.swift
+++ Sources/CollectionViewInteractorInput.swift
@@ -8,16 +8,32 @@
 import CompositionalLayoutViewController
 import UIKit
 
+@resultBuilder
+public enum StoreBuilder {
+    public static func buildBlock(_ items: CollectionViewSection...) -> [CollectionViewSection] {
+        items
+    }
+
+    public static func buildOptional(_ items: [CollectionViewSection]?) -> [CollectionViewSection] {
+        items ?? []
+    }
+}
+
 public protocol CollectionViewInteractorInput: AnyObject {
     var sections: [CollectionViewSection] { get set }
 
     func store(_ sections: [CollectionViewSection])
+    func store(@StoreBuilder _ sections: () -> [CollectionViewSection])
     func section(for sectionIndex: Int) -> CollectionViewSection
 }
 
 public extension CollectionViewInteractorInput {
     func store(_ sections: [CollectionViewSection]) {
         self.sections = sections
+    }
+
+    func store(@StoreBuilder _ sections: () -> [CollectionViewSection]) {
+        self.sections = sections()
     }
 
     func section(for sectionIndex: Int) -> CollectionViewSection {
